cc6214072576f6de6b3ed364fb709bd51f5ac92a
Nathasan1410 <118044230+Nathasan1410@users.noreply.github.com>
2022-11-25 10:07:30 +0700

    Add files via upload

--- 3_A_71220872.py
+++ 3_A_71220872.py
@@ -5,7 +5,8 @@ Umur = int(input('Masukkan umur anda: '))
 poin1 = (no1*50/100)
 poin2 = (no2*30/100)
 poin3 = (no3*20/100)
-ratarata = (poin1+poin2+poin3)
+rata = (poin1+poin2+poin3)
+ratarata = round(rata,2)
 print("Rata-rata nilai anda: "+str(ratarata))
 if Umur<=25:
     if ratarata>=80:
